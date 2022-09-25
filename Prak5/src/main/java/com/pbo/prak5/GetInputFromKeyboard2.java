7db94b13842aab3483f016e5066dd3ac73cf30e5
Kaylan-m <112637404+Kaylan-m@users.noreply.github.com>
2022-09-25 10:04:56 +0700

    Add files via upload

---
+++ Prak5/src/main/java/com/pbo/prak5/GetInputFromKeyboard2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbo.prak5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kayla Nahda Muadzah (21343052)
+ */
+public class GetInputFromKeyboard2 {
+    public static void main(String[] args){
+        String name = "", hoby = " ";
+        
+        name = JOptionPane.showInputDialog("Nama Anda :");
+        hoby = JOptionPane.showInputDialog("Hobi Anda :");
+        
+        String msg = "Jadi Hobi Anda" + hoby + ". "
+                + "Hobi yang bagus " + name;
+                
+        JOptionPane.showMessageDialog(null, msg);
+        
+        System.out.println("Jadi Hobi Anda "+hoby+". Hobi yang bagus" + name);
+        
+    }
+    
+}
+
